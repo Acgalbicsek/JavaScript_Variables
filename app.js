52b5fd562c34fa9bd4a3529032b91614cfc69edf
Amy Galbicsek <acgalbicsek@gmail.com>
2023-04-04 10:32:25 -0800

    JS Var Complete

--- app.js
+++ app.js
@@ -3,8 +3,31 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 and 2
+const firstName = "Amy";
+let lastName = "Galbicsek";
+var age = 36;
 
-// Exercise 2
+lastName = "Corrado";
+age = 26;
+
+
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log('$(laguage) was first released in $(createdYear)');
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 456;
+let hasRead = true;
